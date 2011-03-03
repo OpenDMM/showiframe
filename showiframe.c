76e570405e2331f49b3562272d407b0790210d22
Andreas Oberritter <obi@opendreambox.org>
2011-03-03 12:30:28 +0100

    add GPLv2 header and copyright

--- showiframe.c
+++ showiframe.c
@@ -1,3 +1,14 @@
+/*
+ * showiframe.c
+ *
+ * Copyright (C) 2005 Felix Domke <tmbinc@elitedvb.net>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
